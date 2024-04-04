27d7779d90ee44fddb18f77bd3499a89a604b76d
iremceliksena <iremceliksena@gmail.com>
2024-04-04 17:56:36 +0300

    development branch added

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Developer branch added',
       theme: ThemeData(
         // This is the theme of your application.
         //
