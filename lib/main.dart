e4df09cf4fa9db85ca24f3b9c221c3cf8b873a61
iremceliksena <iremceliksena@gmail.com>
2024-04-05 08:29:37 +0300

    Todo eklendi

--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      // TODO: isimlendirmelerde '' yerine "" kullan
       title: 'Developer branch added',
       theme: ThemeData(
         // This is the theme of your application.
